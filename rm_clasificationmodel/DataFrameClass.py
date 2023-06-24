0176e7b9abd940efe5874e04ad04b82b12e6426f
shoney <shoney.john@outlook.com>
2023-06-24 22:05:49 +0000

    doc

--- rm_clasificationmodel/DataFrameClass.py
+++ rm_clasificationmodel/DataFrameClass.py
@@ -9,18 +9,21 @@ class DataFrame:
         resp=self.es.getFeatures()
 
         for hit in resp['hits']['hits']:
-            list_row =[]
+            list_row = dict (name=None,category=None)
             for name in hit['_source']['nameSource']:
-                if (name["language"]=="en"):                    
-                    list_row.append((name["value"]))     
+                if (name["language"]=="en"):
+                    list_row["name"]=name["value"]
 
     
             for cats in hit['_source']['categoriesSource']:
                 if (cats["language"]=="en"):
-                    list_row.append((cats["label"]))
+                    list_row["category"]=cats["label"]
             
 
-            df_eng.loc[len(df_eng)] = list_row
+            if(list_row):
+                new_row = pd.Series(list_row)
+                df_eng=pd.concat([df_eng, new_row.to_frame().T], ignore_index=True)
+            # df_eng.loc[len(df_eng)] = list_row
 
         return df_eng      
 
