9eafb0ab07a49e006fc0e8c9ab9039f8f33e5319
shoney.arickathil <shoney.arickathil@retromotion.com>
2023-07-06 11:42:36 +0200

    doc

--- rm_clasificationmodel/main.py
+++ rm_clasificationmodel/main.py
@@ -15,32 +15,34 @@ def main():
     es =ElasticSearchDb("http://172.17.212.35:9200")
    
     df=DataFrame(es)
-    dfProductTaxonomyEn=df.getProductTaxonomy(0,10000)    
-    # df_en = df.getNormal()
+    # dfProductTaxonomyEn=df.getProductTaxonomy(0,10000)    
+    df_en = df.getNormal()
     # train = Train(df_en)
-    # train.run(200000,20000,10000)
+    # train.run(300000,30000,3000)
     predict = Predict()
-    # df_prediction=pd.DataFrame(columns=["id",'name','category','predicted','value'])
-    # list_row_en = dict (id=None,name=None,category=None,predicted=None,value=None)
-    list =[]
-    for index, row in dfProductTaxonomyEn.iterrows():
-        predicted =predict.start(row["name"].lower())
 
-        if(predicted[0][1]!=row["category"].lower()):
-            list_row = dict (id=None,name=None,category=None,predicted=None,value=None)
-            list_row["id"] =row["id"]
-            list_row["name"] =row["name"]
-            list_row["category"] =row["category"]
-            list_row["predicted"] =predicted[0][1]
-            list_row["value"] =predicted[0][0]
+    predict.confusionMatix(df_en)
+    # # df_prediction=pd.DataFrame(columns=["id",'name','category','predicted','value'])
+    # # list_row_en = dict (id=None,name=None,category=None,predicted=None,value=None)
+    # list =[]
+    # for index, row in dfProductTaxonomyEn.iterrows():
+    #     predicted =predict.start(row["name"].lower())
 
-            list.append(list_row)
+    #     if(predicted[0][1]!=row["category"].lower()):
+    #         list_row = dict (id=None,name=None,category=None,predicted=None,value=None)
+    #         list_row["id"] =row["id"]
+    #         list_row["name"] =row["name"]
+    #         list_row["category"] =row["category"]
+    #         list_row["predicted"] =predicted[0][1]
+    #         list_row["value"] =predicted[0][0]
+
+    #         list.append(list_row)
 
             
 
-            # new_row = pd.Series(list_row)
-            # df_prediction=pd.concat([df_prediction, new_row.to_frame().T], ignore_index=True)
-    es.createIndexerPrediction(list)
+    #         # new_row = pd.Series(list_row)
+    #         # df_prediction=pd.concat([df_prediction, new_row.to_frame().T], ignore_index=True)
+    # es.createIndexerPrediction(list)
 
 
 if __name__=="__main__":
--- rm_clasificationmodel/predict.py
+++ rm_clasificationmodel/predict.py
@@ -1,6 +1,8 @@
+from matplotlib import ticker
 from NN import *
 import sys
 from helper import *
+from data import *
 
 class Predict():
     def __init__(self):
@@ -9,12 +11,7 @@ class Predict():
 
     def evaluate(self,tensor):
         hidden = self.rnn.initHidden()
-
-            # for i in range(tensor.size()[0]):
-            #     output, hidden = self.rnn(tensor[i], hidden)
         output, hidden = self.rnn(tensor, hidden)
-
-
         return output
 
     def start(self,name, n_predictions=1):
@@ -33,4 +30,49 @@ class Predict():
                     predictions.append([value, self.helper.getCategoryByIndex(category_index)])
             
             return predictions
+    
+    def confusionMatix(self,df_en):
+        # Keep track of correct guesses in a confusion matrix
+        data = Data(df_en)
+        n_categories = len(data.all_category)
+
+        batch = random.choices(data.all_category,k=20)
+        
+        confusion = torch.zeros(n_categories, n_categories)
+        n_confusion = 10000
+
+        # Go through a bunch of examples and record which are correctly guessed
+        for i in range(n_confusion):
+            category, name, category_tensor, name_tensor = data.randomTrainingExample()
+            if category in batch:
+                output = self.evaluate(name_tensor)
+                guess, guess_i = data.categoryFromOutput(output)
+                category_i =data.all_category.index(category)
+                confusion[category_i][guess_i] += 1
+        
+        # Normalize by dividing every row by its sum
+        for i in range(20):
+            confusion[i] = confusion[i] / confusion[i].sum()
+       
+        
+        # Set up plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.matshow(confusion.numpy())
+        fig.colorbar(cax)
+
+        # Set up axes
+        ax.set_xticklabels([''] + data.all_category, rotation=45)
+        ax.set_yticklabels([''] + data.all_category)
+
+        # Force label at every tick
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+        # sphinx_gallery_thumbnail_number = 2
+        plt.show()
+        plt.savefig('confusion.png', dpi=400)
+    
+
+
 
