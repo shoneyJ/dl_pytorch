e9511ad6498a8fb5cdb542b98aeae98b372745f4
shoney.arickathil <shoney.arickathil@retromotion.com>
2023-09-03 14:22:42 +0200

    doc

--- rm_clasificationmodel/TrainClass.py
+++ rm_clasificationmodel/TrainClass.py
@@ -9,12 +9,12 @@ from NN import *
 
 class Train():
     def __init__(self,df_en):
-        self.df_en=df_en  
+        self.df_en=df_en
         self.data= Data(df_en)
         self.inputSize, self.n_category= self.data.getIOSize()
         self.n_hidden = 128
         self.rnn= RNN(self.inputSize, self.n_hidden, self.n_category)
-        
+
         self.lr_low=0.0075
         self.lr_max=0.02
         self.learning_rate= 0.00000000000001
@@ -28,21 +28,21 @@ class Train():
         # Define the number of iterations or epochs for the up and down phases
         self.up_iterations = 1000
         self.down_iterations = 1000
- 
+
 
     def train(self,category_tensor, name_tensor):
-    
+
         hidden = self.rnn.initHidden()
 
         self.rnn.zero_grad()
-        # for i in range(name_tensor.size()[0]):     
+        # for i in range(name_tensor.size()[0]):
 
         #     output, hidden = self.rnn(name_tensor[i], hidden)
 
         output, hidden = self.rnn(name_tensor, hidden)
         loss = self.criterion(output, category_tensor)
         loss.backward()
-       
+
         # Add parameters' gradients to their values, multiplied by learning rate
         for p in self.rnn.parameters():
             p.data.subtract_(p.grad.data, alpha=self.learning_rate)
@@ -50,13 +50,13 @@ class Train():
 
         return output, loss.item()
 
-      
+
     def run(self,n_iters,print_every,plot_every):
         start = time.time()
         for epoch in range(1, n_iters + 1):
-    
+
             category, name, category_tensor, name_tensor = self.data.randomTrainingExample()
-            
+
             output, loss = self.train(category_tensor, name_tensor)
             self.current_loss += loss
              # Print ``iter`` number, loss, name and guess
@@ -64,19 +64,19 @@ class Train():
             if epoch % print_every == 0:
                 guess, guess_i = self.data.categoryFromOutput(output)
                 correct = '✓' if guess == category else '✗ (%s)' % category
-                
-                print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, 
+
+                print('%d %d%% (%s) %.4f %s / %s %s' % (epoch,
                                                 epoch / n_iters * 100,
                                                 self.helper.timeSince(start),
                                                 loss,
-                                                name, 
-                                                guess, 
+                                                name,
+                                                guess,
                                                 correct))
 
             if epoch % plot_every == 0:
                 self.all_losses.append(self.current_loss / plot_every)
                 self.current_loss = 0
-            
+
         self.helper.plot(self.all_losses,"loss.png")
 
         torch.save(self.rnn,'ngram-rnn-classification.pt')
@@ -89,30 +89,30 @@ class Train():
         start = time.time()
 
         for n_iters in batch:
-           
+
             learn_rates=np.linspace(self.lr_low,self.lr_max,n_iters)
             for epoch,lr in zip(range(1, n_iters + 1),learn_rates):
-        
+
                 self.learning_rate=lr
                 self.lr_low=lr
                 category, name, category_tensor, name_tensor = self.data.randomTrainingExample()
-                
+
                 output, loss = self.train(category_tensor, name_tensor)
                 self.current_loss += loss
                 # Print ``iter`` number, loss, name and guess
                 if epoch % 1000 == 0:
                     guess, guess_i = self.data.categoryFromOutput(output)
                     correct = '✓' if guess == category else '✗ (%s)' % category
-                
-                    print('%d %d%% (%s) %.4f %s / %s %s %.10f' % (epoch, 
+
+                    print('%d %d%% (%s) %.4f %s / %s %s %.10f' % (epoch,
                                                 epoch / n_iters * 100,
                                                 self.helper.timeSince(start),
                                                 loss,
-                                                name, 
-                                                guess, 
+                                                name,
+                                                guess,
                                                 correct, self.learning_rate))
 
-             
+
                 if epoch % 1000 == 0:
                     if (math.isnan(self.current_loss)!=True):
                         self.all_losses.append(self.current_loss / 1000)
@@ -122,13 +122,13 @@ class Train():
                         break
 
                 total=total-1
-            
+
         # Plot loss curve
-        
+
         plt.figure()
         plt.plot(learning_rates,self.all_losses)
         plt.ylabel("Loss")
-        plt.xlabel('Learning rate')        
+        plt.xlabel('Learning rate')
         # Title and display the plot
         plt.title('Learning Rates vs. Losses')
         plt.savefig("lossinfo")
@@ -136,11 +136,12 @@ class Train():
 
 
     def runBatchWithTLR(self):
-        batch=[10000,10000,10000,10000,10000,10000,10000,10000,10000,10000]        
+        batch=[10000,10000,10000,10000,10000,10000,10000,10000,10000,10000]
         start = time.time()
         no_batch=0
         no_iterations=0
         learning_rates =[]
+        times =[]
 
         for n_iters in batch:
 
@@ -149,43 +150,51 @@ class Train():
             learn_rates=np.linspace(self.lr_low,self.lr_max,n_iters)
             if isEven==0:
                 learn_rates=np.flip(learn_rates)
-                
+
             for epoch,lr in zip(range(1, n_iters + 1),learn_rates):
                 no_iterations= no_iterations+1
-        
-                self.learning_rate=lr              
+
+                self.learning_rate=lr
                 category, name, category_tensor, name_tensor = self.data.randomTrainingExample()
-                
+
                 output, loss = self.train(category_tensor, name_tensor)
                 self.current_loss += loss
                 # Print ``iter`` number, loss, name and guess
-                if epoch % 5000 == 0:
+                if epoch % 2500 == 0:
                     guess, guess_i = self.data.categoryFromOutput(output)
                     correct = '✓' if guess == category else '✗ (%s)' % category
-                
-                    print('%d %d%% (%s) %.4f %s / %s %s %.10f' % (no_iterations, 
+
+                    print('%d %d%% (%s) %.4f %s / %s %s %.10f' % (no_iterations,
                                                 no_iterations / np.sum(batch) * 100,
                                                 self.helper.timeSince(start),
                                                 loss,
-                                                name, 
-                                                guess, 
+                                                name,
+                                                guess,
                                                 correct, self.learning_rate))
 
-             
-                if epoch % 5000 == 0:
+
+                if epoch % 2500 == 0:
                     if (math.isnan(self.current_loss)!=True):
-                        self.all_losses.append(self.current_loss / 5000)
+                        self.all_losses.append(self.current_loss / 2500)
                         learning_rates.append(self.learning_rate)
+                        times.append(self.helper.timeSince(start))
                         self.current_loss = 0
                     else:
                         break
-            
+
         # Plot loss curve
-        
-        plt.figure()
-        plt.plot(self.all_losses)
-        plt.ylabel("Loss")
-        plt.xlabel('Epoch')        
-        # # Title and display the plot
-        plt.title('Losses Vs No of Epoch')
+        # Initialise the subplot function using number of rows and columns
+        figure, axs = plt.subplots(2,sharex=True)
+        figure.suptitle("Loss")
+        # plt.figure()
+        axs[0].plot(self.all_losses,learning_rates,'tab:orange')
+        axs[1].plot(self.all_losses,times,'tab:green')
+
+        for ax in axs.flat:
+            ax.set(xlabel='Loss')
+
+        # plt.ylabel("Loss")
+        # plt.xlabel('Learning rate')
+        # # # Title and display the plot
+        # ax1.set_title('Losses Vs Learning rate')
         plt.savefig("lossinfo")
